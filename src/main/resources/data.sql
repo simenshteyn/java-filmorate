6cd048f824ff9b594355d9b6a261e8ed3b507e92
Sergey Simenshteyn <simenshteyn@gmail.com>
2022-06-24 15:38:38 +0700

    Add preloaded schema and data
    Took 41 minutes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO genres (genre_name)
+       VALUES ('comedy'),
+              ('drama'),
+              ('cartoon'),
+              ('thriller'),
+              ('documentary'),
+              ('action');
+
+INSERT INTO ratings (rating_name)
+       VALUES ('G'),
+              ('PG'),
+              ('PG-13'),
+              ('R'),
+              ('NC-17');
+
